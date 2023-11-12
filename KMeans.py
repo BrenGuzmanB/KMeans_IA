1647fb17b9d27768637359034d82be8785fc78af
Bren <brenguzmanb@gmail.com>
2023-11-12 01:53:35 -0600

    método predict, simulación de x_test

--- KMeans.py
+++ KMeans.py
@@ -7,27 +7,34 @@ Created on Tue Nov  7 22:36:14 2023
 import numpy as np
 
 class KMeans(object):
-    def __init__(self, k, max_iterations=100, random_seed=None):
+    def __init__(self, k, max_iterations=100, random_seed=3):
+        
         self.k = k
         self.max_iterations = max_iterations
         self.random_seed = random_seed
         np.random.seed(random_seed)
+        self.centroids = None
 
     def initialize_centroids(self, data):
+        
         indices = np.random.choice(data.shape[0], self.k, replace=False)
         centroids = data[indices, :]
         return centroids
 
     def assign_to_clusters(self, data, centroids):
+        
         distances = np.linalg.norm(data[:, np.newaxis, :] - centroids, axis=2)
         labels = np.argmin(distances, axis=1)
         return labels
 
     def update_centroids(self, data, labels):
+        
         centroids = np.array([np.mean(data[labels == i, :], axis=0) for i in range(self.k)])
+        
         return centroids
 
     def fit(self, data):
+        
         centroids = self.initialize_centroids(data)
 
         for iteration in range(self.max_iterations):
@@ -43,6 +50,17 @@ class KMeans(object):
 
         else:
             print(f"Se alcanzó el número máximo de iteraciones ({self.max_iterations}) sin converger.")
-
+            
+        self.centroids = centroids
+        
         return labels
 
+    def predict(self, new_data):
+        
+        if self.centroids is None:
+            raise ValueError("Debe ajustar el modelo utilizando el método fit antes de predecir.")
+
+        # Asignar nuevos datos a los clusters basándose en los centroides almacenados
+        distances = np.linalg.norm(new_data[:, np.newaxis, :] - self.centroids, axis=2)
+        labels = np.argmin(distances, axis=1)
+        return labels
--- Main.py
+++ Main.py
@@ -7,8 +7,13 @@ Created on Tue Nov  7 22:41:16 2023
 
 from Dataframe import Dataframe
 from KMeans import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 #%% CARGAR LOS DATOS
+
+#%%% X Train
 
 # Ruta de los archivos
 training_images_filepath = 'MNIST/t10k-images.idx3-ubyte'
@@ -20,26 +25,110 @@ train_dataframe = Dataframe(training_images_filepath, training_labels_filepath)
 # Cargar los datos
 x_train, y_train = train_dataframe.load_data()
 
-#%% K-Means
+#%%% X Test
+
+# Obtener una muestra aleatoria de 100 registros (simulación de x_test)
+n_muestra = 100
+indices_muestra = np.random.choice(x_train.shape[0], n_muestra, replace=False)
+x_test = x_train[indices_muestra, :]
+y_test = y_train[indices_muestra]
 
 
-kmeans = KMeans(k=10, max_iterations=300, random_seed=3)
-cluster_labels = kmeans.fit(x_train)
+#%% IMPLEMENTACIÓN K-MEANS
 
-#%% sklearn (comparación)
-from sklearn.cluster import KMeans
+#%%% Fit con x_train
 
-kmeans = KMeans(n_clusters=10, random_state=3, n_init="auto").fit(x_train)
-labels = kmeans.labels_
+kmeans = KMeans(k=10, max_iterations=300)
+train_labels = kmeans.fit(x_train)
 
-#%%
-import numpy as np
+#%%% Predict con x_test
 
-np.unique(y_train)
-np.unique(y_train, return_counts=True)
+cluster_labels = kmeans.predict(x_test)
+
+#%%% sklearn (sólo como comparación)
+
+from sklearn.cluster import KMeans as sKMeans
+
+skmeans = sKMeans(n_clusters=10, random_state=3, n_init="auto").fit(x_test)
+labels = skmeans.labels_
 
 
-np.unique(cluster_labels)
-np.unique(cluster_labels, return_counts=True)
 
-#%%
+#%% GRÁFICAS
+
+#%%% Etiquetas reales 
+
+np.random.seed(3)
+
+# Reducción de dimensionalidad con PCA
+n_componentes = 80
+pca = PCA(n_components=n_componentes)
+x_test_reducido = pca.fit_transform(x_test)
+
+# Seleccionar una muestra aleatoria de 1000 registros
+indices_muestra = np.random.choice(x_test_reducido.shape[0], 10, replace=False)
+muestra = x_test_reducido[indices_muestra, :]
+
+muestra_labels = y_test[indices_muestra]
+
+# Graficar la muestra en función de los dos primeros componentes principales
+plt.figure(figsize=(10, 8))
+
+for cluster in range(10):  # 10 clusters
+    indices_cluster = np.where(muestra_labels == cluster)
+    plt.scatter(
+        muestra[indices_cluster, 0],
+        muestra[indices_cluster, 1],
+        label=f'Cluster {cluster}',
+        alpha=0.7
+    )
+
+plt.title('Muestra Aleatoria de 1000 Registros: Etiquetas Reales')
+plt.xlabel('Primer Componente Principal')
+plt.ylabel('Segundo Componente Principal')
+plt.legend()
+plt.show()
+
+#%%% K-Means clustering
+
+muestra_labels = cluster_labels[indices_muestra]
+
+# Graficar la muestra en función de los dos primeros componentes principales
+plt.figure(figsize=(10, 8))
+
+for cluster in range(10):  # 10 clusters
+    indices_cluster = np.where(muestra_labels == cluster)
+    plt.scatter(
+        muestra[indices_cluster, 0],
+        muestra[indices_cluster, 1],
+        label=f'Cluster {cluster}',
+        alpha=0.7
+    )
+
+plt.title('Muestra Aleatoria de 1000 Registros: K-Means')
+plt.xlabel('Primer Componente Principal')
+plt.ylabel('Segundo Componente Principal')
+plt.legend()
+plt.show()
+
+#%%% Sklearn (Sólo como prueba)
+
+muestra_labels = labels[indices_muestra]
+
+# Graficar la muestra en función de los dos primeros componentes principales
+plt.figure(figsize=(10, 8))
+
+for cluster in range(10):  # 10 clusters
+    indices_cluster = np.where(muestra_labels == cluster)
+    plt.scatter(
+        muestra[indices_cluster, 0],
+        muestra[indices_cluster, 1],
+        label=f'Cluster {cluster}',
+        alpha=0.7
+    )
+
+plt.title('Muestra Aleatoria de 1000 Registros: Sklearn')
+plt.xlabel('Primer Componente Principal')
+plt.ylabel('Segundo Componente Principal')
+plt.legend()
+plt.show()
